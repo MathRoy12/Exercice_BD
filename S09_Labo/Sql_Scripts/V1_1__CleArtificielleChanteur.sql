e799c090fc4e86c675ebe2ac1d26f810f84680a7
Mathieu Roy <2234476@cegepmontpetit.ca>
2024-03-25 10:52:39 -0400

    PROGRESS R17

---
+++ S09_Labo/Sql_Scripts/V1_1__CleArtificielleChanteur.sql
@@ -0,0 +1,38 @@
+
+	-- Nouvelle colonne pour la PK de Musique.Chanteur et la FK de Musique.Chanson
+ALTER TABLE  Musique.Chanson
+ADD ChanteurID int
+GO
+ALTER TABLE Musique.Chanteur
+ADD ChanteurID int IDENTITY (1,1)
+GO
+	-- Supprimer les anciennes contraintes FK puis PK (attention, l'ordre de suppression est important ici)
+ALTER TABLE Musique.Chanson
+DROP CONSTRAINT FK_Chanson_NomChanteur
+GO
+ALTER TABLE Musique.Chanteur
+DROP CONSTRAINT PK_Chanteur_Nom
+GO
+
+	-- Nouvelles contraintes PK puis FK
+ALTER TABLE Musique.Chanteur
+ADD CONSTRAINT PK_Chanteur_ChanteurID PRIMARY KEY (ChanteurID)
+GO
+
+ALTER TABLE Musique.Chanson
+ADD CONSTRAINT FK_Chanson_ChanteurID FOREIGN KEY (ChanteurID) REFERENCES Musique.Chanteur(ChanteurID)
+GO
+	
+	-- Remplir la nouvelle colonne FK et faire en sorte que le nouveau champ que vous avez crée ChanteurID n'est pas null maintenant
+	
+UPDATE Musique.Chanson
+SET Chanson.ChanteurID = Chanteur.ChanteurID 
+FROM Musique.Chanson
+INNER JOIN Musique.Chanteur ON Chanteur.Nom = Chanson.NomChanteur
+GO
+    
+	-- Supprimer l'ancienne colonne FK de Musique.Chanson (On veut garder le nom des chanteurs, donc on ne supprime pas l'ancienne PK !)
+	
+ALTER TABLE Musique.Chanson
+DROP COLUMN NomChanteur
+	
---
+++ S09_Labo/Sql_Scripts/V1_2__VueChanteurs.sql
@@ -0,0 +1,16 @@
+-- Nouvelle vue
+
+CREATE VIEW Musique.VW_ChanteurNbChansons AS
+SELECT CHT.ChanteurID, CHT.Nom, CHT.DateNaissance, COUNT(CHS.ChanteurID) AS [Nombre de chansons]
+FROM Musique.Chanteur CHT
+         INNER JOIN Musique.Chanson CHS ON CHT.ChanteurID = CHS.ChanteurID
+GROUP BY CHT.ChanteurID, CHT.Nom, CHT.DateNaissance
+	
+-- ?
+
+GO
+
+-- Résultat souhaité : id du chanteur, nom du chanteur, date de naissance et son nombre de chansons
+
+-- ChanteurID |Nom  | Date de naissance |Nombre de chansons
+-- -----------|-----|-------------------|-------------------
